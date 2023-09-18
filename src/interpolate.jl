ed5c2632fe20a308b21c8635e9eeb53635869f4d
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-18 16:30:58 -0300

    Add default estimator

--- src/interpolate.jl
+++ src/interpolate.jl
@@ -14,6 +14,27 @@
     )
   
 TODO
+
+## Global Parameters
+
+* `minneighbors` - Minimum number of neighbors (default to `1`)
+* `maxneighbors` - Maximum number of neighbors (default to `10`)
+* `neighborhood` - Search neighborhood (default to `nothing`)
+* `distance`     - A distance defined in Distances.jl (default to `Euclidean()`)
+* `exponent`     - Exponent of the distances (default to `1`)
+* `path`         - The path algorithm used to iterate over the domain (default to `LinearPath()`)
+
+The `maxneighbors` option can be used to perform inverse distance weighting
+with a subset of measurements per prediction location. If `maxneighbors`
+is not provided, then all measurements are used.
+
+Two `neighborhood` search methods are available:
+
+* If a `neighborhood` is provided, local prediction is performed 
+  by sliding the `neighborhood` in the domain.
+
+* If a `neighborhood` is not provided, the prediction is performed 
+  using `maxneighbors` nearest neighbors according to `distance`.
 """
 struct Interpolate{D<:Domain,N,M,P} <: TableTransform
   domain::D
@@ -46,6 +67,9 @@ Interpolate(
   path
 )
 
+Interpolate(domain::Domain; distance=Euclidean(), kwargs...) =
+  Interpolate(domain, [AllSpec()], [IDW(1, distance)]; distance, kwargs...)
+
 Interpolate(domain::Domain, pairs::Pair{<:Any,<:GeoStatsModel}...; kwargs...) =
   Interpolate(domain, colspec.(first.(pairs)), last.(pairs); kwargs...)
 
@@ -65,7 +89,7 @@ function apply(transform::Interpolate, geotable::AbstractGeoTable)
   neighborhood = transform.neighborhood
   distance = transform.distance
   path = transform.path
-  
+
   nobs = nrow(geotable)
   if maxneighbors > nobs || maxneighbors < 1
     @warn "Invalid maximum number of neighbors. Adjusting to $nobs..."
@@ -82,7 +106,7 @@ function apply(transform::Interpolate, geotable::AbstractGeoTable)
   # preprocess variable models
   varmodels = mapreduce(vcat, colspecs, models) do colspec, model
     svars = choose(colspec, vars)
-    svars .=> model
+    svars .=> Ref(model)
   end
 
   # pre-allocate memory for neighbors
@@ -96,23 +120,23 @@ function apply(transform::Interpolate, geotable::AbstractGeoTable)
     map(inds) do ind
       # centroid of estimation
       center = centroid(idom, ind)
-  
+
       # find neighbors with data
       nneigh = search!(neighbors, center, searcher)
-  
+
       # skip if there are too few neighbors
       if nneigh < minneighbors
         missing
       else
         # final set of neighbors
         ninds = view(neighbors, 1:nneigh)
-  
+
         # view neighborhood with data
         samples = view(geotable, ninds)
-  
+
         # fit model to data
         fmodel = fit(model, samples)
-  
+
         # save prediction
         predict(fmodel, var, idom[ind])
       end
