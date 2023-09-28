adae2a268fb86c2e55d18a9450ef111aa2153e74
Elias Carvalho <eliascarvdev@gmail.com>
2023-09-28 15:32:51 -0300

    Add more constructors for 'Interpolate' and 'InterpolateNeighbors'

--- src/interpneighbors.jl
+++ src/interpneighbors.jl
@@ -4,13 +4,16 @@
 
 """
     InterpolateNeighbors(domain, vars₁ => model₁, ..., varsₙ => modelₙ; [parameters])
+    InterpolateNeighbors([g₁, g₂, ..., gₙ], vars₁ => model₁, ..., varsₙ => modelₙ; [parameters])
   
-Interpolate geospatial data on given `domain` using geostatistical models
-`model₁`, ..., `modelₙ` for variables `vars₁`, ..., `varsₙ`.
+Interpolate geospatial data on given `domain` or set of geometries `g₁`, `g₂`, ..., `gₙ`,
+using geostatistical models `model₁`, ..., `modelₙ` for variables `vars₁`, ..., `varsₙ`.
 
     InterpolateNeighbors(domain, model=IDW(); [parameters])
+    InterpolateNeighbors([g₁, g₂, ..., gₙ], model=IDW(); [parameters])
   
-Interpolate geospatial data on given `domain` using geostatistical `model` for all variables.
+Interpolate geospatial data on given `domain` or set of geometries `g₁`, `g₂`, ..., `gₙ`,
+using geostatistical `model` for all variables.
 
 Unlike [`Interpolate`](@ref), this transform uses neighbor search methods to
 fit geostatistical models at each interpolation location with a reduced number
@@ -73,10 +76,13 @@ InterpolateNeighbors(
   prob
 )
 
-InterpolateNeighbors(domain::Domain, model::GeoStatsModel=IDW(); kwargs...) =
+InterpolateNeighbors(geoms::AbstractVector{<:Geometry}, colspecs, models; kwargs...) =
+  InterpolateNeighbors(GeometrySet(geoms), colspecs, models; kwargs...)
+
+InterpolateNeighbors(domain, model::GeoStatsModel=IDW(); kwargs...) =
   InterpolateNeighbors(domain, [AllSpec()], [model]; kwargs...)
 
-InterpolateNeighbors(domain::Domain, pairs::Pair{<:Any,<:GeoStatsModel}...; kwargs...) =
+InterpolateNeighbors(domain, pairs::Pair{<:Any,<:GeoStatsModel}...; kwargs...) =
   InterpolateNeighbors(domain, colspec.(first.(pairs)), last.(pairs); kwargs...)
 
 isrevertible(::Type{<:InterpolateNeighbors}) = false
