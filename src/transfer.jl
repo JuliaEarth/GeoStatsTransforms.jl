a56d20eb9a1c93700671fa13dda385dcbe81d7bc
Elias Carvalho <eliascarvdev@gmail.com>
2024-03-25 15:43:59 -0300

    Update docstring

--- src/transfer.jl
+++ src/transfer.jl
@@ -6,7 +6,7 @@
     Transfer(domain)
     Transfer([g₁, g₂, ..., gₙ])
 
-Transfer variables `var₁`, `var₂`, ..., `varₙ` from original domain to geospatial `domain`.
+Transfer variables `var₁`, `var₂`, ..., `varₙ` from source domain to target `domain`.
 Alternatively, transfer variables to geometries `g₁`, `g₂`, ..., `gₙ`.
 
 # Examples
