8fdd9dc8ac0fe833512c707c0a657a8f42e58c85
Elias Carvalho <eliascarvdev@gmail.com>
2024-03-25 14:18:07 -0300

    Update tests

--- test/aggregate.jl
+++ test/aggregate.jl
@@ -22,17 +22,11 @@
   @test ngtb.b[2] == last(gtb.b[[4, 5]])
   @test ngtb.b[3] == last(gtb.b[[1, 2]])
 
-  grid1 = CartesianGrid((0.0, 0.0), (10.0, 10.0), dims=(10, 10))
-  grid2 = CartesianGrid((0.0, 0.0), (10.0, 10.0), dims=(20, 20))
-  gtb = georef((a=rand(Float64, 100), b=rand(Int, 100)), grid1)
+  grid1 = CartesianGrid((0.0, 0.0), (10.0, 10.0), dims=(20, 20))
+  grid2 = CartesianGrid((0.0, 0.0), (10.0, 10.0), dims=(10, 10))
+  gtb = georef((a=rand(Float64, 400), b=rand(Int, 400)), grid1)
   ngtb = gtb |> Aggregate(grid2)
   @test domain(ngtb) == grid2
-  @test count(!ismissing, ngtb.a) == 100
-  @test count(!ismissing, ngtb.b) == 100
-
-  gtb = georef((a=rand(Float64, 400), b=rand(Int, 400)), grid2)
-  ngtb = gtb |> Aggregate(grid1)
-  @test domain(ngtb) == grid1
   @test ngtb[(1, 1), :a] == mean(gtb[(1:2, 1:2), :a])
   @test ngtb[(1, 10), :a] == mean(gtb[(1:2, 19:20), :a])
   @test ngtb[(10, 1), :a] == mean(gtb[(19:20, 1:2), :a])
@@ -41,4 +35,15 @@
   @test ngtb[(1, 10), :b] == first(gtb[(1:2, 19:20), :b])
   @test ngtb[(10, 1), :b] == first(gtb[(19:20, 1:2), :b])
   @test ngtb[(10, 10), :b] == first(gtb[(19:20, 19:20), :b])
+
+  ngtb = gtb |> Aggregate(grid2, :a => median, :b => last)
+  @test domain(ngtb) == grid2
+  @test ngtb[(1, 1), :a] == median(gtb[(1:2, 1:2), :a])
+  @test ngtb[(1, 10), :a] == median(gtb[(1:2, 19:20), :a])
+  @test ngtb[(10, 1), :a] == median(gtb[(19:20, 1:2), :a])
+  @test ngtb[(10, 10), :a] == median(gtb[(19:20, 19:20), :a])
+  @test ngtb[(1, 1), :b] == last(gtb[(1:2, 1:2), :b])
+  @test ngtb[(1, 10), :b] == last(gtb[(1:2, 19:20), :b])
+  @test ngtb[(10, 1), :b] == last(gtb[(19:20, 1:2), :b])
+  @test ngtb[(10, 10), :b] == last(gtb[(19:20, 19:20), :b])
 end
