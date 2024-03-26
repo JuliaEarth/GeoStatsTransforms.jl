ef81e1d587c82d4ac473f7dc14ba04ced15b970d
Elias Carvalho <eliascarvdev@gmail.com>
2024-03-26 16:28:31 -0300

    Add tests

--- test/downscale.jl
+++ test/downscale.jl
@@ -22,4 +22,26 @@
   @test ngtb[(19, 20), :b] == gtb[(10, 10), :b]
   @test ngtb[(20, 19), :b] == gtb[(10, 10), :b]
   @test ngtb[(20, 20), :b] == gtb[(10, 10), :b]
+
+  rgrid = convert(RectilinearGrid, grid)
+  trgrid = convert(RectilinearGrid, tgrid)
+  gtb = georef((a=rand(Float64, 100), b=rand(Int, 100)), rgrid)
+  ngtb = gtb |> Downscale(2, 2)
+  @test domain(ngtb) == trgrid
+  @test ngtb[(1, 1), :a] == gtb[(1, 1), :a]
+  @test ngtb[(1, 2), :a] == gtb[(1, 1), :a]
+  @test ngtb[(2, 1), :a] == gtb[(1, 1), :a]
+  @test ngtb[(2, 2), :a] == gtb[(1, 1), :a]
+  @test ngtb[(1, 19), :a] == gtb[(1, 10), :a]
+  @test ngtb[(1, 20), :a] == gtb[(1, 10), :a]
+  @test ngtb[(2, 19), :a] == gtb[(1, 10), :a]
+  @test ngtb[(2, 20), :a] == gtb[(1, 10), :a]
+  @test ngtb[(19, 1), :b] == gtb[(10, 1), :b]
+  @test ngtb[(19, 2), :b] == gtb[(10, 1), :b]
+  @test ngtb[(20, 1), :b] == gtb[(10, 1), :b]
+  @test ngtb[(20, 2), :b] == gtb[(10, 1), :b]
+  @test ngtb[(19, 19), :b] == gtb[(10, 10), :b]
+  @test ngtb[(19, 20), :b] == gtb[(10, 10), :b]
+  @test ngtb[(20, 19), :b] == gtb[(10, 10), :b]
+  @test ngtb[(20, 20), :b] == gtb[(10, 10), :b]
 end
