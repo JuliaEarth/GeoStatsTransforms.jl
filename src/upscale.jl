0b2d31796c37b3f90e76775c47aa66edb3481b2f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2024-03-26 10:42:49 -0300

    Update src/upscale.jl

--- src/upscale.jl
+++ src/upscale.jl
@@ -10,6 +10,9 @@ Upscale each dimension of the grid by given factors `f₁`, `f₂`, ..., `fₙ`.
 This transform is equivalent to skipping entries of the grid
 as in the pseudo-code `grid[1:f₁:end, 1:f₂:end, ..., 1:fₙ:end]`.
 
+Resulting values are obtained with the [`Aggregate`](@ref) transform
+and its default aggregation functions.
+
 # Examples
 
 ```julia
